da56a5390366043353ab86b768409c52749b829e
Bypass007 <676707528@qq.com>
2023-06-09 10:00:23 +0800

    first commit

--- demo/app.js
+++ demo/app.js
@@ -3,7 +3,7 @@ let OSS = require('ali-oss');
 let client = new OSS({
 
   region: 'oss-cn-hangzhou',
-  accessKeyId: 'LTAI5tDqZjgNQ2HCbi6h2nHJ',
-  accessKeySecret: 'DemoForTestZyA4QlPvr7iLH8fCavJ',
+  accessKeyId: 'LTAI5tDqZjgNQ2HCbi6h2nHa',
+  accessKeySecret: 'DemoForTestZyA4QlPvr7iLH8fCaaJ',
   
 });
